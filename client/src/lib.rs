c46824f8da66d88066606f99e7491acaf74ff0d4
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2022-07-15 01:12:08 +0900

    fix: ページ構成を変更

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -2,11 +2,7 @@
 
 use seed::{prelude::*, *};
 
-mod example_a;
-mod example_b;
-mod example_c;
-mod example_d;
-mod example_e;
+mod app_home;
 
 // ------ ------
 //     Init
@@ -22,11 +18,7 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
 
 #[derive(Default)]
 struct Model {
-    example_a: example_a::Model,
-    example_b: example_b::Model,
-    example_c: example_c::Model,
-    example_d: example_d::Model,
-    example_e: example_e::Model,
+    app_home: app_home::Model,
 }
 
 // ------ ------
@@ -34,29 +26,13 @@ struct Model {
 // ------ ------
 
 enum Msg {
-    ExampleA(example_a::Msg),
-    ExampleB(example_b::Msg),
-    ExampleC(example_c::Msg),
-    ExampleD(example_d::Msg),
-    ExampleE(example_e::Msg),
+    AppHome(app_home::Msg),
 }
 
 fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
-        Msg::ExampleA(msg) => {
-            example_a::update(msg, &mut model.example_a, &mut orders.proxy(Msg::ExampleA));
-        }
-        Msg::ExampleB(msg) => {
-            example_b::update(msg, &mut model.example_b, &mut orders.proxy(Msg::ExampleB));
-        }
-        Msg::ExampleC(msg) => {
-            example_c::update(msg, &mut model.example_c, &mut orders.proxy(Msg::ExampleC));
-        }
-        Msg::ExampleD(msg) => {
-            example_d::update(msg, &mut model.example_d, &mut orders.proxy(Msg::ExampleD));
-        }
-        Msg::ExampleE(msg) => {
-            example_e::update(msg, &mut model.example_e, &mut orders.proxy(Msg::ExampleE));
+        Msg::AppHome(msg) => {
+            app_home::update(msg, &mut model.app_home, &mut orders.proxy(Msg::AppHome));
         }
     }
 }
@@ -72,11 +48,7 @@ fn view(model: &Model) -> impl IntoNodes<Msg> {
             St::MaxWidth => px(460);
             St::Margin => "auto";
         },
-        example_a::view(&model.example_a, view_intro).map_msg(Msg::ExampleA),
-        example_b::view(&model.example_b, view_intro).map_msg(Msg::ExampleB),
-        example_c::view(&model.example_c, view_intro).map_msg(Msg::ExampleC),
-        example_d::view(&model.example_d, view_intro).map_msg(Msg::ExampleD),
-        example_e::view(&model.example_e, view_intro).map_msg(Msg::ExampleE),
+        app_home::view(&model.app_home, view_intro).map_msg(Msg::AppHome),
     ]
 }
 
